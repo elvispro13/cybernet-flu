671a07bcec41495d5fdca5076fb16f49bb99ca75
Elvis Martinez <elvispro13@gmail.com>
2023-11-24 21:00:17 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(const MyApp());
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cybernet',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cybernet Pay '),
+        ),
+        body: const Center(
+          child: Text('Probando Cybernet Pay'),
+        ),
+      ),
+    );
+  }
+}
