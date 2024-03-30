a6c72d7c52e5489e28a0c7fb26f10ef26c6d9eda
Elvis Martinez <elvispro13@gmail.com>
2024-03-30 01:10:53 -0600

    mm

---
+++ lib/services/saldos_service.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:cybernet/global/environment.dart';
+import 'package:http/http.dart' as http;
+
+class SaldosService {
+
+  Future<List<SaldoView>> (String usuario, String password) async {
+    
+  }
+}
